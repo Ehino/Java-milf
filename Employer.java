641b10533708395e1cc82ec9c09639a538c8de87
vaditel <artem.ychaikin@gmail.com>
2025-12-06 05:52:30 +0700

    Fixed Employer Class

--- Employer.java
+++ Employer.java
@@ -3,8 +3,11 @@ public abstract class Employer{
 
     public static int getId() { return id; }
 
+    protected String name;
     protected String password;
 
+
+    protected String city;
     protected String companyName;
     protected String jobDescribe;
     protected String girlType;
@@ -15,9 +18,11 @@ public abstract class Employer{
         this.id = id;
     }
 
-    public Employer(String password, String companyName, String jobDescribe,String girlType, String requirements, boolean advertStatus){
+    public Employer(String name, String password,String city, String companyName, String jobDescribe,String girlType, String requirements, boolean advertStatus){
         Employer.id ++;
+        this.name = name;
         this.password = password;
+        this.city = city;
         this.jobDescribe = jobDescribe;
         this.companyName = companyName;
         this.girlType = girlType;
