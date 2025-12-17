f5bdc9a9c255e702c1c83f814d28f5aadd97ef5b
Elnur <elnurgasymov2006@gmail.com>
2025-12-18 00:03:56 +0700

    Начала реализации SQL

---
+++ Configs.java
@@ -0,0 +1,9 @@
+public class Configs {
+	
+	protected String dbHost = "localhost";
+	protected String dbPort = "3306";
+	protected String dbUser = "root";
+	protected String dbPass = "93Bomolu!)";
+	protected String dbName = "DirtyCirls";
+
+}
---
+++ Const.java
@@ -0,0 +1,11 @@
+public class Const {
+	public static final String ALT_TABLE = "Altushka";
+	public static final String ALT_ID = "idAltushka";
+	public static final String ALT_NAME = "name";
+	public static final String ALT_CITY = "city";
+	public static final String ALT_AGE = "age";
+	public static final String ALT_COOKING = "cooking";
+	public static final String ALT_BOYFRIEND = "countBoyfriend";
+	public static final String ALT_PASSWORD = "password";
+	public static final String ALT_GIRLTYPE = "girltype";
+}
